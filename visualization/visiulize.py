4702a3f0122fbb8a4d0b0837710e16383de624b5
ItsJamin <jamonjamin4@gmail.com>
2024-11-02 20:00:46 +0100

    changed messy path handling

--- visualization/visiulize.py
+++ visualization/visiulize.py
@@ -1,10 +1,9 @@
 import sys
 import os
 
-# HAUPTVERZEICHNIS IST BEI JEDEM ANDERS!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), r"C:\Users\Yunus\Documents\GitHub\SWP Anwendungen von Algos\cg-shop-25")))
+sys.path.append(os.getcwd())
 
-from inpout.loadsave import load_problem
+from inpout import load_problem
 from instance import plot_instance
 import matplotlib.pyplot as plt
 
