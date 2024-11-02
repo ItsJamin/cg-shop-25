d26d9aec25a3f722445a46c6512c9c7d41902ea3
ItsJamin <jamonjamin4@gmail.com>
2024-11-02 18:51:39 +0100

    Revert "removed pycache *2"

--- visualization/visiulize.py
+++ visualization/visiulize.py
@@ -1,14 +1,17 @@
 import sys
 import os
 
+# HAUPTVERZEICHNIS IST BEI JEDEM ANDERS!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), r"C:\Users\Yunus\Documents\GitHub\SWP Anwendungen von Algos\cg-shop-25")))
 
-from .inpout import instance as i
+from inpout.loadsave import load_problem
+from instance import plot_instance
 import matplotlib.pyplot as plt
 
 
-#problem_instance = n.load_problem("test.json")
+problem_instance = load_problem("test.json")
 
 
 fig, ax = plt.subplots()
-plot_instance(ax, None)#problem_instance)
+plot_instance(ax, problem_instance)
 plt.show()
