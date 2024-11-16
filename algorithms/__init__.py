9f0c0d03f192395b6b0e003b9d26f54a53b5f275
ItsJamin <jamonjamin4@gmail.com>
2024-11-16 14:02:30 +0100

    some thoughts

--- algorithms/__init__.py
+++ algorithms/__init__.py
@@ -1,1 +1,2 @@
 from .greedy_algorithm import *
+from .greedy_top_bottom import greedy_top_bottom
--- algorithms/greedy_top_bottom.py
+++ algorithms/greedy_top_bottom.py
@@ -2,6 +2,9 @@ from instance import Problem, Result
 import geometry as geo
 
 def greedy_top_bottom(problem : Problem) -> Result:
+
+    result = Result()
+
     #erstelle sortierte liste, angefangen mit dem höchsten punkt
 
     #erstelle Funktion die überprüft ob eine kante eine schon existieren kante überschneidet
@@ -11,4 +14,21 @@ def greedy_top_bottom(problem : Problem) -> Result:
     #For schleife die Punkte in der Liste durchlaufen und dabei die oben genannten Funktionen aufrufen
     #Falls beides True -> erstelle Kante
     #Falls einer von beiden False -> ignoriere Punkt und mache mit nächten weiter
-    
+
+    #for point in points:
+        
+    #    if (_no_edge_intersection() and _edge_in_boundary()):
+    #        result.edges.append(edge)
+
+    pass
+
+
+
+def _sort_points_bottom_up():
+    pass
+
+def _no_edge_intersection():
+    pass
+
+def _edge_in_boundary():
+    pass
