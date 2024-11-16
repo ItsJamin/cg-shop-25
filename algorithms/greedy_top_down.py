ccb93f48a265d91e65f349283b671efae49e380d
ItsJamin <jamonjamin4@gmail.com>
2024-11-16 15:21:43 +0100

    first version of greedy top down

--- algorithms/greedy_top_down.py
+++ algorithms/greedy_top_down.py
@@ -5,20 +5,25 @@ def greedy_top_down(problem : Problem) -> Result:
 
     result = Result()
 
+    all_edges = problem.g_constraints + problem.g_region_boundary.edges()
+
     #erstelle sortierte liste, angefangen mit dem höchsten punkt
+    points = _sort_points_top_down(problem.g_points)
 
-    #erstelle Funktion die überprüft ob eine kante eine schon existieren kante überschneidet
+    for index, point in enumerate(points):
 
-    #erstelle Funktion ob Kante innerhalb der Bounderie ist
+        for prev_point in points[:index]:
+            
+            temp_edge = geo.create_full_edge(point, prev_point)
+            print(point, prev_point, temp_edge)
 
-    #For schleife die Punkte in der Liste durchlaufen und dabei die oben genannten Funktionen aufrufen
-    #Falls beides True -> erstelle Kante
-    #Falls einer von beiden False -> ignoriere Punkt und mache mit nächten weiter
-
-    #for point in points:
-        
-    #    if (_no_edge_intersection() and _edge_in_boundary()):
-    #        result.edges.append(edge)
+            if _no_edge_intersection(temp_edge, all_edges) and _edge_in_boundary(temp_edge, problem.g_region_boundary):
+                
+                # Edge ist okay
+                all_edges.append(temp_edge)
+                problem.step(temp_edge, color="orange")
+                # TODO: verkettung der edge mit dem rest.
+            
 
     return result
 
@@ -35,7 +40,7 @@ def _sort_points_top_down(liste : list[geo.Vertex]):
                 liste[i], liste[j] = liste[j], liste[i]
     return liste
 
-def _no_edge_intersection(new_edge, existing_edges):
+def _no_edge_intersection(new_edge : geo.HalfEdge, existing_edges : list[geo.HalfEdge]):
     """
     Überprüft ob eine neue Kante mit bereits existierenden Kanten sich überschneiden.
     """
@@ -49,6 +54,8 @@ def _edge_in_boundary(edge : geo.HalfEdge, boundary: geo.Face):
     Überprüft das eine Kante in einer Fläche liegt. True wenn in der Fläche.
     Annahme: Keine Punkte ausserhalb der boundary.
     """
+
+    # TODO: Falsch weil linie kann auch innerhalb der boundary sein
     
     # Checkt den Fall das beide Punkte zum Außenbereich gehören
     if edge.origin in boundary.vertices and edge.twin.origin in boundary.vertices:
--- demonstration.py
+++ demonstration.py
@@ -4,10 +4,10 @@ import geometry as geo
 import algorithms as alg
 
 if __name__ == '__main__':
-    problem = inp.load_problem("cgshop2025_examples_simple-polygon-exterior-20_80_23272e96.instance.json")
+    problem = inp.load_problem("test.json")
     
     # Führe den Greedy-Algorithmus aus
-    alg.greedy_triangulation(problem)
+    alg.greedy_top_down(problem)
     
     # Animation starten
     vis.animate_algorithm(problem, interval=500)
