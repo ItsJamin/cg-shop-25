036a58892be5171c8c71113cebc04ef5a48dd5a5
ItsJamin <103188307+ItsJamin@users.noreply.github.com>
2024-11-16 20:07:11 +0100

    updated demonstration.py

--- demonstration.py
+++ demonstration.py
@@ -4,10 +4,12 @@ import geometry as geo
 import algorithms as alg
 
 if __name__ == '__main__':
+
+    # Erstelle das Problem aus der JSON-Datei
     problem = inp.load_problem("cgshop2025_examples_ortho_20_b099d1fe.instance.json")
     
     # Führe den Greedy-Algorithmus aus
     solution = alg.greedy_top_down(problem)
     
-    # Animation starten
+    # Visualisiere das Problem und animiere was der Algorithmus erstellt hat. 
     vis.animate_algorithm(problem, solution, interval=800)
